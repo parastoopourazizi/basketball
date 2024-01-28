a49ae657b153ec5aee1095633e6e4bb0f3ec2d7c
parastoopourazizi <napourazizi@gmail.com>
2024-01-28 13:59:38 +0330

    add page add-news

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,27 @@ import About from "./pages/About";
 import History from "./pages/History";
 import Picture from "./pages/Picture";
 import Members from "./pages/Members";
-import News from "./pages/News";
+import News from "./pages/news/News";
+import AddNews from "./pages/addNews/AddNews";
 import Login from "./components/Login/Login";
 import Register from "./components/register/Register";
 
 function App() {
-
   return (
-
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/About" element={<About/>}/>
-    <Route path="/History" element={<History/>}/>
-    <Route path="/Members" element={<Members/>}/>
-    <Route path="/News" element={<News/>}/>
-    <Route path="/Picture" element={<Picture/>}/> 
-    <Route path="/Login" element={<Login/>}/>
-    <Route path="/Register" element={<Register />} />
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/History" element={<History />} />
+        <Route path="/Members" element={<Members />} />
+        <Route path="/News" element={<News />} />
+        <Route path="add-news" element={<AddNews />} />
+        <Route path="/Picture" element={<Picture />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
     </BrowserRouter>
-    
- 
-    
-     
-    
-  )
+  );
 }
 
-
-
-export default App
+export default App;
---
+++ src/pages/addNews/AddNews.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+
+export default function AddNews() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
