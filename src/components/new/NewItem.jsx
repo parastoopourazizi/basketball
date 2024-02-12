7aae0c97c85d50240b61e922c030ef04a7429af3
parastoopourazizi <napourazizi@gmail.com>
2024-02-12 13:17:38 +0330

    add api news

--- src/components/new/NewItem.jsx
+++ src/components/new/NewItem.jsx
@@ -6,13 +6,16 @@ function NewItem() {
       <div className=" max-w-sm rounded overflow-hidden shadow-lg">
         <img
           className="w-[377px] h-[244px] origin-top-left rotate-180"
-          src="./assets/images/image232.png"
+          src={props.image}
         />
         <div className="w-[360px] h-[49px] text-right text-gray-900 text-lg font-bold font-['Montserrat'] leading-[25.20px]">
-          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+          {props.context}
         </div>
         <div className="w-[142px] text-right text-zinc-500 text-sm font-normal font-['Open Sans'] leading-snug">
-          1 مهر 1402
+          {props.created_at}
+        </div>
+        <div className="w-[142px] text-right text-zinc-500 text-sm font-normal font-['Open Sans'] leading-snug">
+          {props.updated_at}
         </div>
       </div>
     </div>
--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,11 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NewItem from "../../components/new/NewItem";
 import axios from "axios";
 
 function News() {
+  const [article , setArticle] = useState({
+    id : "",
+    image : "",
+    context : "",
+    created_at : "",
+    updated_at : "",
+  });
+
   useEffect(() =>{
     axios
+    .get("https://bushehrbasketball.pythonanywhere.com/basketball/news/")
+    .then((response) => setArticle(response.data));
 
   }, []);
   return (
@@ -13,10 +23,9 @@ function News() {
       <Navbar />
       <div>
         <div className="container  flex grid grid-cols-3 ">
-          <NewItem />
-          <NewItem />
-          <NewItem />
-          <NewItem />
+          {article.map((article) => (
+            <NewItem {...article}/>
+          ))}
         </div>
         <button>بیشتر</button>
       </div>
