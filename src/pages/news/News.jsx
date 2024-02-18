e0e38c1388d18b60a4535242a5d0fd46334eeddf
parastoopourazizi <napourazizi@gmail.com>
2024-02-18 12:44:08 +0330

    news

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -22,21 +22,16 @@ function News() {
     <div>
       <Navbar />
       <div>
-<<<<<<< HEAD
+
         <div className="container  flex grid grid-cols-3 ">
           {article.map((article) => (
             <NewItem {...article}/>
           ))}
-=======
-        <div className="container grid grid-cols-3 ">
-          <NewItem />
-          <NewItem />
-          <NewItem />
-          <NewItem />
->>>>>>> cfbe5af7725bd55d0e2503f480176bc75b18781c
-        </div>
+
+       
         <button>بیشتر</button>
       </div>
+    </div>
     </div>
   );
 }
